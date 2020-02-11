d2ed89385e6cd52b6d1d1c7106f5e902bd6114b4
Gustavo Landfried <gustavolandfried@gmail.com>
2020-02-11 08:56:38 -0300

    Add flag into wget commandline

--- download.sh
+++ download.sh
@@ -2,14 +2,14 @@
 
 if [ $# \> 0 ]
 then
-    sed -n "/$1/,/^}/p" biblio.bib | grep url | grep -o '{.*}' | sed  's/{/"/g' | sed 's/}/"/g' | xargs wget --adjust-extension -O download/$1
+    sed -n "/$1/,/^}/p" biblio.bib | grep url | grep -o '{.*}' | sed  's/{/"/g' | sed 's/}/"/g' | xargs wget -U --adjust-extension -O download/$1
 else
     items=$(cat biblio.bib | grep @ | awk -F '{' '{print $2}' | awk -F ',' '{print $1}')
     for i in $items
     do
         if [ ! -f "download/$i" ]
 	then
-	    sed -n "/$i/,/^}/p" biblio.bib | grep url | grep -o '{.*}' | sed  's/{/"/g' | sed 's/}/"/g' | xargs wget --adjust-extension -O download/$i
+	    sed -n "/$i/,/^}/p" biblio.bib | grep url | grep -o '{.*}' | sed  's/{/"/g' | sed 's/}/"/g' | xargs wget -U --adjust-extension -O download/$i
         fi
     done
     echo "All biblio download"
