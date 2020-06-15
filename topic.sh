50c6c91bdb768099bf6814f62ba6e0b8e63a2915
Gustavo Landfried <gustavolandfried@gmail.com>
2020-06-15 10:28:10 -0300

    topic downloader

--- topic.sh
+++ topic.sh
@@ -1,4 +1,7 @@
 cat $1 | while read line
 do
+    if [ ! -f "download/$line" ]
+    then
 	sed -n "/$line/,/^}/p" biblio.bib | grep url | grep -o '{.*}' | sed  's/{/"/g' | sed 's/}/"/g' | xargs wget -U --adjust-extension -O download/$line
+    fi
 done
